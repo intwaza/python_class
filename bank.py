ddb0f7469dfab9979db2c4822f2731d9d7411a90
intwaza <iintwazabelyse@gmail.com>
2021-05-24 19:46:43 +0300

    pythonclass

--- bank.py
+++ bank.py
@@ -1,9 +1,31 @@
 class BankAccount:
-    def __init__(self,accountName,balance):
-        self.accountName=accountName
-        self.balance=balance
-    def Deposit(self,amount):
-        return f"Dear customer {self.accountName}, you have deposit {amount} now your balance is {self.balance+amount}"
-    def withdraw(self,amount):
-        return f"Dear customer {self.accountName}, you have withdraw {amount} now your balance is {self.balance-amount}"
+    def __init__(self,name,phone_number):
+        self.name=name
+        self.phone_number=phone_number
+        self.balance=0
+        self.loan=0
 
+    def show_balance(self):
+        return f"Dear customer {self.name} your balance is {self.balance} ksh"
+
+    def deposit(self,amount):
+        
+        if amount > 0:
+            self.balance+=amount
+            return self.show_balance()
+        else:
+            return f"Dear customer {self.name} your amount is negative"
+
+    def withdraw(self,amount):
+        if self.balance <amount:
+            return f"Dear customer {self.name}, you don't have enough money to your account"
+        else:
+            self.balance-=amount
+            return self.show_balance()
+
+    def borrow(self,amount):
+        self.loan+=amount
+        return f"Dear customer {self.name} you have borrowed {self.loan} ksh"
+    def repay(self,amount):
+        self.loan-=amount
+        return f"Dear customer {self.name} you loan is {self.loan} ksh you have paid {amount}"
--- student.py
+++ student.py
@@ -1,9 +1,14 @@
 class Student:
     school="AkiraChix"
-    def __init__(self,name,age):
-        self.name=name
+    def __init__(self,first_name,last_name,age):
+        self.first_name=first_name
+        self.last_name=last_name
         self.age=age
     def speak(self):
         return f"Hello class, My name is {self.name}"
     def year_of_birth(self):
-        return f"I was born {2021-self.age}"
+        return f"Hello {self.first_name} {self.last_name},you was born {2021-self.age}"
+    def greet(self):
+        return f"Hello {self.first_name} welcome to {self.school}"
+    def initials(self):
+        return f"Hello {self.first_name} {self.last_name} your initials are {self.first_name[0]}{self.last_name[0]}"
