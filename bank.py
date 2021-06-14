d7caf1b306aefbae077279e004755714fb2658f4
intwaza <iintwazabelyse@gmail.com>
2021-06-14 20:04:04 +0300

    pythonclass

--- bank.py
+++ bank.py
@@ -1,4 +1,4 @@
-from datetime import datetime, time
+from datetime import datetime
 class BankAccount:
     def __init__(self,name,phone_number):
         self.name=name
@@ -11,6 +11,10 @@ class BankAccount:
         return f"Dear customer {self.name} your balance is {self.balance} ksh"
 
     def deposit(self,amount):
+        try:
+            10+amount
+        except TypeError:
+            return f"Dear customer {self.name} the amount must be figures"
         if amount > 0:
             self.balance+=amount
             now= datetime.now()
@@ -29,6 +33,10 @@ class BankAccount:
       
 
     def withdraw(self,amount):
+        try:
+            10+amount
+        except TypeError:
+            return f"Dear customer {self.name} the amount must be figures"
         if self.balance <amount:
             return f"Dear customer {self.name}, you don't have enough money to your account"
         else:
@@ -44,6 +52,10 @@ class BankAccount:
 
 
     def borrow(self,amount):
+        try:
+            10+amount
+        except TypeError:
+            return f"Dear customer {self.name} the amount must be figures"
         limit=10/100
         if amount<0:
             return "sorry we can't give you this loan it is too loan"
@@ -65,7 +77,12 @@ class BankAccount:
             self.statement.append(transaction)
 
             return f"Dear customer {self.name} you have borrowed {self.loan} ksh"
+
     def repay(self,amount):
+        try:
+            10+amount
+        except TypeError:
+            return f"Dear customer {self.name} the amount must be figures"
         if amount<0:
             return "Dear customer your payment is too low"
         elif amount<=self.loan:
@@ -80,11 +97,27 @@ class BankAccount:
             transaction={
                 "amount":amount,
                 "time":now,
-                "Narration":"you have repayed your loan"
+                "Narration":"you have repaid your loan"
             }
             self.statement.append(transaction)
 
             return f"Dear customer {self.name} you have paid your loan and your new balance is {self.balance}"
+    
+    def transfer(self,account,amount):
+        try:
+            10+amount
+        except TypeError:
+            return f"Dear customer {self.name} the amount must be figures"
+        fee= amount*0.05
+        Total=fee+amount
+        if amount<0:
+            return f"Dear customer {self.name} your amount is too low"
+        elif Total>self.balance:
+            return f"Dear customer {self.name} you balance is {self.balance} and you need atleast {Total}"
+        else:
+            self.balance-=Total
+            account.deposit(amount)
+            return f"Dear customer you  have sent {amount} to {account.name}"
 
     def showStatement(self):
         for transaction in self.statement:
@@ -94,3 +127,21 @@ class BankAccount:
             date= time.strftime("%d/%m/%y")
             print(f"{date}: {Narration} {amount}")
         return 
+
+class MobileMoneyAccount(BankAccount):
+    def __init__(self, name, phone_number,service_provider):
+        BankAccount.__init__(name, phone_number)
+        self.service_provider= service_provider
+    
+    def buy_airtime(self,amount):
+        try:
+            10+amount
+        except TypeError:
+            return f"Dear customer {self.name} the amount must be figures"
+        if amount<0:
+            return f"Dear customer {self.name} you amount {amount} is too low"
+        elif self.balance<amount:
+            return f"Dear customer your balance is {self.balance} is too low you can't buy airtime"
+        else:
+            self.balance-=amount
+            return f"Dear customer you have bought airtime of {amount}, your new balance is {self.balance}"
